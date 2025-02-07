aecb815c0b6e76f3a731d81bc43d3e2ec5192ca0
mujin0137 <anwls5928@naver.com>
2025-02-07 16:30:06 +0900

    feat: TodoList 컴포넌트에 상태 관리 및 제출 기능 추가가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,6 @@
-const todos = [
+import { useState } from "react";
+
+const SAMPLE_TODOS = [
   { id: 1, text: "Buy milk" },
   { id: 2, text: "Clean the house" },
   { id: 3, text: "Go for a run" },
@@ -12,12 +14,36 @@ const todos = [
 ];
 
 const TodoList = () => {
+  const [todos, setTodos] = useState(SAMPLE_TODOS);
+  const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!todoText.trim()) {
+      return;
+    }
+
+    setTodos([{ id: crypto.randomUUID(), text: todoText}, ...todos]);
+
+    setTodoText("")
+  };
+
+  const handleChangeTodoText = (e) => {
+    setTodoText(e.target.value)
+  }
   return (
-    <ul>
-      {todos.map((todo) => 
-         (<li key={todo.id}>{todo.text}</li>)
-      )}
-    </ul>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todoText} onChange={handleChangeTodoText}/>
+        <button type="submit">제출하기</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
