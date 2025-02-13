04c7bc2391a37a65e92131297827a8eba9338230
mujin0137 <anwls5928@naver.com>
2025-02-13 16:08:04 +0900

    feat: TodoList 컴포넌트에 완료 상태 토글 기능 추가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 const SAMPLE_TODOS = [
-  { id: 1, text: "Buy milk" },
-  { id: 2, text: "Clean the house" },
-  { id: 3, text: "Go for a run" },
-  { id: 4, text: "Finish homework" },
-  { id: 5, text: "Call mom" },
-  { id: 6, text: "Buy groceries" },
-  { id: 7, text: "Walk the dog" },
-  { id: 8, text: "Read a book" },
-  { id: 9, text: "Do laundry" },
-  { id: 10, text: "Write code" },
+  { id: 1, text: "Buy milk", completed: false },
+  { id: 2, text: "Clean the house", completed: false },
+  { id: 3, text: "Go for a run", completed: false },
+  { id: 4, text: "Finish homework", completed: false },
+  { id: 5, text: "Call mom", completed: false },
+  { id: 6, text: "Buy groceries", completed: false },
+  { id: 7, text: "Walk the dog", completed: false },
+  { id: 8, text: "Read a book", completed: false },
+  { id: 9, text: "Do laundry", completed: false },
+  { id: 10, text: "Write code", completed: false },
 ];
 
 const TodoList = () => {
@@ -24,23 +24,48 @@ const TodoList = () => {
       return;
     }
 
-    setTodos([{ id: crypto.randomUUID(), text: todoText}, ...todos]);
+    setTodos([{ id: crypto.randomUUID(), text: todoText }, ...todos]);
 
-    setTodoText("")
+    setTodoText("");
   };
 
   const handleChangeTodoText = (e) => {
-    setTodoText(e.target.value)
-  }
+    setTodoText(e.target.value);
+  };
+
+  const handleToggleCompleted = (id) => {
+    const updatedTodos = todos.map((todo)=> {
+      if(todo.id === id ) {
+        const newTodo = {
+          ...todo,
+          completed: !todo.completed
+        }
+
+        return newTodo;
+      } else {
+        return todo;
+      }
+    });
+
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={todoText} onChange={handleChangeTodoText}/>
+        <input type="text" value={todoText} onChange={handleChangeTodoText} />
         <button type="submit">제출하기</button>
       </form>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+        {todos.map(({ id, text, completed}) => (
+          <li key={id}>
+            <p
+            style={{
+              textDecoration: completed ? "line-through" : "none",
+              }}
+              >{text}</p>
+            <button onClick={() => handleToggleCompleted(id)}>{completed ? "취소하기" : "확인하기"} </button>
+          </li>
         ))}
       </ul>
     </div>
